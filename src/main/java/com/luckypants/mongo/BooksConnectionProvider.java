896b7630eb2638da61513b33adc5c2e457c9ae8f
jszew2 <jszew2@unh.newhaven.edu>
2014-05-30 21:12:17 -0400

    commit

---
+++ src/main/java/com/luckypants/mongo/BooksConnectionProvider.java
@@ -0,0 +1,60 @@
+package com.luckypants.mongo;
+
+import java.net.UnknownHostException;
+
+import com.luckypants.properties.PropertiesLookup;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+
+public class BooksConnectionProvider {
+	/**
+	 * TODO:modify this method to allow passing the collection name to it
+	 * @return
+	 */
+
+	public DBCollection getCollection() {
+		try {
+
+			MongoClient mongo = new MongoClient("oceanic.mongohq.com", 10076);
+
+			DB db = mongo.getDB("LuckyPantsjszew2");
+			if (db == null) {
+				System.out.println("Could not connect to Database");
+			}
+
+			boolean auth = db.authenticate("jszew2", "jszew2".toCharArray());
+			if (auth == false) {
+				System.out.println("Could not authenticate");
+			}
+
+			DBCollection booksColl = db.getCollection("books");
+			PropertiesLookup pl = new PropertiesLookup();
+			String mongoUrl = pl.getProperty("oceanic.mongohq.com");
+			String mongoDbPort = pl.getProperty("10076");
+
+			return booksColl;
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public static void main(String[] args) {
+		BooksConnectionProvider books = new BooksConnectionProvider();
+		DBCollection booksCollection = books.getCollection();
+		if(booksCollection == null){
+			System.out.println("ERROR:No Connection");
+		}
+		else{
+			System.out.println("SUCCESS:Connected");
+		}
+
+	}
+
+}
